1b2a23f72aa95d1e2d491170353a1e990359afd9
Pavu Brar <pavubrar@gmail.com>
2026-06-20 19:00:14 -0600

    removed backend causing auth issue due to policy

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+  value = module.resource_group.resource_group_name
 }
 
 output "resource_group_location" {
-  value = azurerm_resource_group.rg.location
+  value = module.resource_group.resource_group_location
 }
