8a7df332be4405443b98647ae77938b0eb8ceefa
Parveen Kaur <pavubrar@gmail.com>
2026-07-21 21:32:00 +0000

    add container environment now adding Managed identity fro conatiner apps

--- environments/dev-aca/main.tf
+++ environments/dev-aca/main.tf
@@ -20,3 +20,14 @@ module "container_app_environment" {
   location            = module.resource_group.resource_group_location
   resource_group_name = module.resource_group.resource_group_name
 }
+module "managed_identity" {
+  source = "../../modules/managed_identity"
+  name                = "bookshelf-api-identity"
+  location            = module.resource_group.resource_group_location
+  resource_group_name = module.resource_group.resource_group_name}
+
+resource "azurerm_role_assignment" "acr_pull" {
+  principal_id         = identity.principal_id
+  role_definition_name = "AcrPull"
+  scope                = acr.id
+}
