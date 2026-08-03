735685720ee8e8b5a461b9553ae7f467a0542dbd
Parveen Kaur <pavubrar@gmail.com>
2026-08-03 17:21:21 +0000

    refactor format for probes blocks

--- modules/container_app_api/main.tf
+++ modules/container_app_api/main.tf
@@ -22,6 +22,42 @@ resource "azurerm_container_app" "api" {
   image  = var.image
   cpu    = 0.5
   memory = "1Gi"
+
+  startup_probe {
+  transport = "HTTP"
+
+  port = 8080
+  path = "/health"
+
+  interval_seconds = 5
+  timeout          = 5
+
+  failure_count_threshold = 30
+}
+readiness_probe {
+  transport = "HTTP"
+
+  port = 8080
+  path = "/health"
+
+  interval_seconds = 10
+  timeout          = 5
+
+  failure_count_threshold = 3
+  success_count_threshold = 1
+}
+liveness_probe {
+  transport = "HTTP"
+
+  port = 8080
+  path = "/health"
+
+  interval_seconds = 30
+  timeout          = 5
+
+  failure_count_threshold = 3
+}
+
   env {
         name  = "ASPNETCORE_ENVIRONMENT"
         value = "Production"
@@ -65,40 +101,6 @@ resource "azurerm_container_app" "api" {
 }
  }
  
-}
-startup_probe {
-  transport = "HTTP"
-
-  port = 8080
-  path = "/health"
-
-  interval_seconds = 5
-  timeout          = 5
-
-  failure_count_threshold = 30
-}
-readiness_probe {
-  transport = "HTTP"
-
-  port = 8080
-  path = "/health"
-
-  interval_seconds = 10
-  timeout          = 5
-
-  failure_count_threshold = 3
-  success_count_threshold = 1
-}
-liveness_probe {
-  transport = "HTTP"
-
-  port = 8080
-  path = "/health"
-
-  interval_seconds = 30
-  timeout          = 5
-
-  failure_count_threshold = 3
 }
 
   ingress {
